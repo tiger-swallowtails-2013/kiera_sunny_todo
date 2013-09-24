d6afd46f0970a57cb8c32f4fe07e9c560c3b9fcb
Apprentice <apprentice@devbootcamp.com>
2013-09-23 20:15:39 -0700

    skeleton

--- app/models/contact.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../../db/config'
-
-class Contact< ActiveRecord::Base
-
-  def self.exist?(input_email)
-    self.find_by(email: input_email) != nil
-  end
-
-end
--- app/test.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../db/config.rb'
-require_relative 'models/contact.rb'
-
-# params[:email]
-
-p Contact.find_by(email:'sunnyisthebest@rosafddsfckin.com') != nil
-
